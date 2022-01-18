33bfa570c67ceccc9e0670c92394efe85ce58b35
James O'Brien <james.obrien@r3.com>
2022-01-18 23:36:19 +0000

    impl - holding for key management

--- src/functions/createCustomer/handler.ts
+++ src/functions/createCustomer/handler.ts
@@ -3,13 +3,9 @@ import { formatJSONResponse } from "@libs/apiGateway";
 import { middyfy } from "@libs/lambda";
 import { APIGatewayProxyEvent } from "aws-lambda";
 import Stripe from "stripe";
-const stripe = new Stripe(
-	"sk_test_51KFG8gFe3afnmlSvPXBZNTmDiggMyoTiKpU5G0CGgSF8jrz9ZsZ1JrAfO75ZBzDB1Cgp4MJELRL79y6NYCfghjda00bs7mll5N" ||
-		"",
-	{
-		apiVersion: "2020-08-27",
-	}
-);
+const stripe = new Stripe("enter key" || "", {
+	apiVersion: "2020-08-27",
+});
 interface IBody {
 	email: string;
 	username: string;
